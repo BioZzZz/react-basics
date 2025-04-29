4558cf5c3f0eeb21485e4abb657b7134a21e424e
Admin <admin@MacBook-Pro-Denis.local>
2025-04-29 21:16:32 +0300

    hw is done

--- src/app/(restaurants)/dish/[dishId]/page.js
+++ src/app/(restaurants)/dish/[dishId]/page.js
@@ -1,11 +1,11 @@
-import { DishPage as RestaurantDishPage } from "../../../../components/pages/dishPage";
 import { getDishById } from "../../../../services/get-dish-by-id";
+import { Dish } from "../../../../components/Dish/dish";
 
 const DishPage = async ({ params }) => {
   const { dishId } = await params;
   const data = await getDishById(dishId);
 
-  return <RestaurantDishPage dish={data} />;
+  return <Dish dish={data} />;
 };
 
 export default DishPage;
--- src/components/pages/dishPage.jsx
+++
@@ -1,5 +0,0 @@
-import { Dish } from "../Dish/dish";
-
-export const DishPage = ({ dish }) => {
-  return <Dish dish={dish} />;
-};
